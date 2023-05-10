ad925384afd6c4aa281dcbd6c95a120c4d889130
matomugo60 <matomugo60@gmail.com>
2023-05-10 09:31:51 +0300

    created apiKeys.js

--- src/apiKeys.js
+++
@@ -1,5 +0,0 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
-module.exports = {
-  key: "df72f9cbb40e3739e12b9709d1c0e63c",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
